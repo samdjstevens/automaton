f307ba8b968fd8f66843d6fa37aeb1a0d3b35375
Sam Stevens <samdjstevens@googlemail.com>
2019-10-28 19:57:57 +0000

    remove unused PayloadRequestTransformingException constructor

--- src/main/java/dev/samstevens/automaton/payload/PayloadRequestTransformingException.java
+++ src/main/java/dev/samstevens/automaton/payload/PayloadRequestTransformingException.java
@@ -1,11 +1,6 @@
 package dev.samstevens.automaton.payload;
 
 public class PayloadRequestTransformingException extends Exception {
-
-    public PayloadRequestTransformingException(String message) {
-        super(message);
-    }
-
     public PayloadRequestTransformingException(Throwable cause) {
         super(cause);
     }
